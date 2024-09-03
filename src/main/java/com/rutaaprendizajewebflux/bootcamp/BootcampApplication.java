5672e64170e2104d126b751a63250665f2b7c67a
Carlos Santarrosa <carlos.santarrosa@pragma.com.co>
2024-09-03 11:50:30 -0500

    first commit

---
+++ src/main/java/com/rutaaprendizajewebflux/bootcamp/BootcampApplication.java
@@ -0,0 +1,13 @@
+package com.rutaaprendizajewebflux.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rutaaprendizajewebflux/bootcamp/BootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rutaaprendizajewebflux.bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
